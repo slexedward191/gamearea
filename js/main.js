6c85881e79c7da1f7a11762fa6e039a1717a726e
slexedward191 <slexedward191@gmail.com>
2026-03-02 09:41:46 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("GameArea ready.");
+const bgm = document.getElementById("bgm");
+const hint = document.getElementById("soundHint");
+
+async function startMusic() {
+  try {
+    await bgm.play();
+    hint.style.display = "none";
+    document.removeEventListener("click", startMusic);
+  } catch (e) {
+    console.log("Müzik başlatılamadı:", e);
+  }
+}
+
+document.addEventListener("click", startMusic);
+hint.addEventListener("click", startMusic);
